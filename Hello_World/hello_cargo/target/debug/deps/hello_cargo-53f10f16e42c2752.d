e49691c78fb62defd35998aad4714c9808ea1975
saurabhsigma <saurabh962003@gmail.com>
2024-10-26 06:28:50 +0530

    first chapter done, done with cargo

---
+++ Hello_World/hello_cargo/target/debug/deps/hello_cargo-53f10f16e42c2752.d
@@ -0,0 +1,5 @@
+/Users/saurabhsingh/Desktop/rust/Hello_World/hello_cargo/target/debug/deps/hello_cargo-53f10f16e42c2752: src/main.rs
+
+/Users/saurabhsingh/Desktop/rust/Hello_World/hello_cargo/target/debug/deps/hello_cargo-53f10f16e42c2752.d: src/main.rs
+
+src/main.rs:
---
+++ Hello_World/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/saurabhsingh/Desktop/rust/Hello_World/hello_cargo/target/debug/hello_cargo: /Users/saurabhsingh/Desktop/rust/Hello_World/hello_cargo/src/main.rs
